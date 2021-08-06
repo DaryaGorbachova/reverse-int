f3bddf763858f18c06f36f5c25e345bf698cd7dd
DaryaGorbachova <vrindavaneshvari@mail.ru>
2021-08-06 13:57:33 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    num = num.toString();
+    let i = num.length - 1;
+    let result = '';
+        while (i >= 0) {
+        result = result + num[i];
+        i = i - 1;
+        }
+        result = Number.parseInt(result);
+        return result;
 }
